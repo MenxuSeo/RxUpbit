d5de4893a54cc0be4291d01f02e68e0a432a14f9
m0su <sirm0su@gmail.com>
2021-03-07 22:16:28 +0900

    add Coin model

---
+++ RxUpbit/Coin/CoinModel.swift
@@ -0,0 +1,18 @@
+//
+//  CoinModel.swift
+//  RxUpbit
+//
+//  Created by seo on 2021/03/07.
+//
+
+struct Coin: Codable {
+  let englishName: String
+  let koreanName: String
+  let market: String
+  
+  enum CodingKeys: String, CodingKey {
+    case englishName = "english_name"
+    case koreanName = "korean_name"
+    case market
+  }
+}
